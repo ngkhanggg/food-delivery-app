572cbbf94eca8f524f3b02f53c13279aa462e37e
ngkhanggg <nguyenkhangchu2004@gmail.com>
2023-12-08 06:33:49 -0500

    Added button's onclick and fixed image bug

--- app/src/main/java/com/example/fooddeliveryapp/MainActivity.java
+++ app/src/main/java/com/example/fooddeliveryapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fooddeliveryapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSignUpButton(View view) {
+        System.out.println("Sign up button clicked");
+    }
+
+    public void onLoginButton(View view) {
+        System.out.println("Login button clicked");
+    }
+
+    public void onGuestButton(View view) {
+        System.out.println("Continue as guest button clicked");
     }
 }
